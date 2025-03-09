cd66dc890c1dba0944529435ffddafade86dc1c1
Ryan Rui <ryanruirr@gmail.com>
2025-03-09 09:34:02 +0800

    update yazi config

---
+++ userhome/.config/yazi/init.lua
@@ -0,0 +1,6 @@
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi
+-- ya pack -a yazi-rs/plugins:git
+require("git"):setup()
+
+-- https://github.com/yazi-rs/plugins/tree/main/mount.yazi
+-- ya pack -a yazi-rs/plugins:mount
