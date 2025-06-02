22fbfa67244ec8a1940871d53af8a44234b3a343
Ryan Rui <ryanruirr@gmail.com>
2025-06-02 10:25:18 +0800

    update config
    1. update yazi plugins
    2. change default terminal to kitty for amazing cursor trail animation

--- userhome/.config/yazi/init.lua
+++ userhome/.config/yazi/init.lua
@@ -4,3 +4,7 @@ require("git"):setup()
 
 -- https://github.com/yazi-rs/plugins/tree/main/mount.yazi
 -- ya pack -a yazi-rs/plugins:mount
+
+-- https://github.com/boydaihungst/simple-mtpfs.yazi
+-- ya pack -a boydaihungst/simple-mtpfs
+require("simple-mtpfs"):setup({})
